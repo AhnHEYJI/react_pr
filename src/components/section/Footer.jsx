72125784509ac545dec7b967243969e40b3778a7
AhnHEYJI <dkwlgpwl11@naver.com>
2024-09-06 20:16:25 +0900

    메인컨텐츠작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer id='footer' role='contentinfo'>
-        Footer</footer>
+        <a href='dkwlgpwl11@naver.com' rel='noopener noreferrer'>
+        dkwlgpwl11@naver.com
+        </a>
+    </footer>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import Today from '../components/contents/Today'
+import Developer from '../components/contents/Developer'
+import Music from '../components/contents/Music'
 
 const Home = () => {
   return (
     <Main title = "Ahn My 유튜브 채널" 
     description="유튜브 채널 방문을 환영합니다.">
-      Home
+
+     <Today />
+     <Developer />
+     <Music />
+
+   
     </Main>
   )
 }
